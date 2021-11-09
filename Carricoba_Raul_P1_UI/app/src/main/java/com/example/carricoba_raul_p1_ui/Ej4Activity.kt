a22508a43407ac1f2cb22eb230a7ab1033050024
Raúl Carricoba Gómez <a20raulcg@ATEIS.LOCAL>
2021-11-09 20:31:15 +0000

    P1_UI act1-4 done

--- Carricoba_Raul_P1_UI/app/src/main/java/com/example/carricoba_raul_p1_ui/Ej4Activity.kt
+++ Carricoba_Raul_P1_UI/app/src/main/java/com/example/carricoba_raul_p1_ui/Ej4Activity.kt
@@ -2,10 +2,28 @@ package com.example.carricoba_raul_p1_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.carricoba_raul_p1_ui.databinding.ActivityEj4Binding
 
 class Ej4Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityEj4Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityEj4Binding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_ej4)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {v -> onClickButton(v)}
+        binding.button2.setOnClickListener {v -> onClickButton(v)}
+        binding.button3.setOnClickListener {v -> onClickButton(v)}
+    }
+
+    private fun onClickButton(v: View) {
+        val but = v as Button
+        Toast.makeText(this, v.text, Toast.LENGTH_SHORT).show()
     }
 }
