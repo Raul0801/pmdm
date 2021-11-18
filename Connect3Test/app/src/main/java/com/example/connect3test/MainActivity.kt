c302e9cbf71b4c0a52b551499dfdea76e7098aea
Raúl Carricoba Gómez <a20raulcg@ateis.local>
2021-11-18 18:51:43 +0000

    Fixed and improved a lot of code in Connect3Test

--- Connect3Test/app/src/main/java/com/example/connect3test/MainActivity.kt
+++ Connect3Test/app/src/main/java/com/example/connect3test/MainActivity.kt
@@ -13,65 +13,64 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    // 0: yellow, 1: red, 2: empty
-    var gameState = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
-    var winningPositions = arrayOf(
+    // 0 = yellow circle, 1 = red circle, 2 = empty space
+    private var gameState = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
+    private var winningPositions = arrayOf(
+        intArrayOf(0, 3, 6),
+        intArrayOf(1, 4, 7),
+        intArrayOf(0, 4, 8),
+        intArrayOf(2, 4, 6),
         intArrayOf(0, 1, 2),
         intArrayOf(3, 4, 5),
         intArrayOf(6, 7, 8),
-        intArrayOf(0, 3, 6),
-        intArrayOf(1, 4, 7),
-        intArrayOf(2, 5, 8),
-        intArrayOf(0, 4, 8),
-        intArrayOf(2, 4, 6)
+        intArrayOf(2, 5, 8)
     )
-    var activePlayer = 0
-    var gameActive = true
+    private var activePlayer = 0
+    private var gameActive = true
 
-    fun onDrawCircle(view: View) {
+    private fun onDrawCircle(view: View) {
         val counter = view as ImageView
         val tappedCounter = counter.tag.toString().toInt()
         if (gameState[tappedCounter] == 2 && gameActive) {
             gameState[tappedCounter] = activePlayer
             counter.translationY = -1500f
             activePlayer = if (activePlayer == 0) {
-                counter.setImageResource(R.drawable.yellow)
+                counter.setImageResource(R.drawable.yellow_circle)
                 1
             } else {
-                counter.setImageResource(R.drawable.red)
+                counter.setImageResource(R.drawable.red_circle)
                 0
             }
             counter.animate().translationYBy(1500f).rotation(3600f).duration = 300
             for (winningPosition in winningPositions) {
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-                    //Someone won
+                    //Someone has won
                     gameActive = false
-                    var winner = ""
-                    winner = if (activePlayer == 1) {
+                    val winner : String = if (activePlayer == 1) {
                         "Yellow"
                     } else {
                         "Red"
                     }
-                    val playAgainButton = findViewById(R.id.playAgainButton) as Button
-                    val winnerTextView = findViewById(R.id.winnerTextView) as TextView
-                    winnerTextView.text = "$winner has won!"
+                    val playAgainButton = findViewById<Button>(R.id.playAgainButton)
+                    val winnerTextView = findViewById<TextView>(R.id.winnerTextView)
+                    "$winner is the winner!".also { winnerTextView.text = it }
                     playAgainButton.visibility = View.VISIBLE
                     winnerTextView.visibility = View.VISIBLE
                 }
             }
             if(!gameState.contains(2)) {
-                val playAgainButton = findViewById(R.id.playAgainButton) as Button
+                val playAgainButton = findViewById<Button>(R.id.playAgainButton)
                 playAgainButton.visibility = View.VISIBLE
             }
         }
     }
 
-    fun restartGame(view: View?) {
-        val playAgainButton = findViewById(R.id.playAgainButton) as Button
-        val winnerTextView = findViewById(R.id.winnerTextView) as TextView
+    private fun restartGame() {
+        val playAgainButton = findViewById<Button>(R.id.playAgainButton)
+        val winnerTextView = findViewById<TextView>(R.id.winnerTextView)
         playAgainButton.visibility = View.INVISIBLE
         winnerTextView.visibility = View.INVISIBLE
-        val gridLayout = findViewById(R.id.gridLayout) as GridLayout
+        val gridLayout = findViewById<GridLayout>(R.id.gridLayout)
         for (i in 0 until gridLayout.childCount) {
             val counter = gridLayout.getChildAt(i) as ImageView
             counter.setImageDrawable(null)
@@ -83,7 +82,7 @@ class MainActivity : AppCompatActivity() {
         gameActive = true
     }
 
-    protected override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
@@ -98,6 +97,6 @@ class MainActivity : AppCompatActivity() {
         binding.imageView7.setOnClickListener {v -> onDrawCircle(v)}
         binding.imageView8.setOnClickListener {v -> onDrawCircle(v)}
         binding.imageView9.setOnClickListener {v -> onDrawCircle(v)}
-        binding.playAgainButton.setOnClickListener {v -> restartGame(v)}
+        binding.playAgainButton.setOnClickListener {restartGame()}
     }
 }
